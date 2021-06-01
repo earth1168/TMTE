7f3551330b4d6de4be33fcaf70ae3047eb691481
Earth <pimanus1234@gmail.com>
2021-06-01 14:44:17 +0700

    Update mediaAd.blade.php

--- TMTE/resources/views/mediaAd.blade.php
+++ TMTE/resources/views/mediaAd.blade.php
@@ -242,7 +242,7 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Cards</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Media Admin</h1>
                     </div>
 
                     <div class="row">
